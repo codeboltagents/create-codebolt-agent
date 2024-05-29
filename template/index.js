5f4bccdd3ed67e7834117d5cad753515b2f5643a
ravi18 <ravi@arrowai.com>
2024-05-29 12:27:45 +0530

    changes in template

--- template/index.js
+++ template/index.js
@@ -1,1 +1,12 @@
 import { codebolt } from '@codebolt/codeboltjs';
+
+// Function to execute codebolt connection
+async function execute() {
+
+    await codebolt.waitForConnection();
+}
+
+// Immediately invoked function expression to execute the code
+(async () => {
+    await execute();
+})();
