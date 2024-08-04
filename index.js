bfb5542dd18537b8c839724c146ebc671d97618d
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2024-08-04 18:30:55 +0530

    fix yaml

--- index.js
+++ index.js
@@ -21,39 +21,219 @@ projectName = options.name || process.argv[2];
 const templateDir = path.resolve(__dirname, 'template');
 const templates = fs.readdirSync(templateDir).filter(file => fs.statSync(path.join(templateDir, file)).isDirectory());
 
-inquirer.prompt([
+// Sample metadata object
+const metadata = {
+  agent_routing: {
+    worksonblankcode: true,
+    worksonexistingcode: true,
+    softwaredevprocessmanaged: [
+      'projectplanning',
+      'codegeneration',
+      'codetesting',
+      'codeoptimization',
+    ],
+    supportedlanguages: [
+      'all',
+      'javascript',
+      'typescript',
+      'python',
+      'go',
+      'ruby',
+    ],
+    supportedframeworks: [
+      'all',
+      'nextjs',
+      'reactjs',
+      'nodejs',
+      'express',
+      'django',
+    ],
+  },
+  defaultagentllm: {
+    strict: true,
+    modelorder: ['ollama2'],
+  },
+  sdlc_steps_managed: [
+    {
+      name: 'codegeneration',
+      example_instructions: [
+        'Generate a new React component',
+        'Create a new API endpoint',
+      ],
+    },
+    {
+      name: 'deployment',
+      example_instructions: [
+        'deploy this file to cloudflare',
+        'deploy this file to firebase',
+      ],
+    },
+  ],
+  llm_role: [
+    {
+      name: 'documentationllm',
+      description: 'LLM to be used for advanced Documentation. Please select a model that excels in documentation tasks.',
+      strict: true,
+      modelorder: [
+        'gpt-4-turbo',
+        'gpt-3.5-turbo',
+        'mistral7b.perplexity',
+        'mistral7b.any',
+        'llama2-70b',
+        'llama2-15b',
+        'group.documentationmodels',
+      ],
+    },
+    {
+      name: 'testingllm',
+      description: 'LLM to be used for advanced Testing. Please select a model that excels in testing tasks.',
+      strict: true,
+      modelorder: [
+        'gpt-4-turbo',
+        'gpt-3.5-turbo',
+        'mistral7b.perplexity',
+        'mistral7b.any',
+        'llama2-70b',
+        'llama2-15b',
+        'group.testingmodels',
+      ],
+    },
+    {
+      name: 'actionllm',
+      description: 'LLM to be used for executing advanced actions.',
+      strict: true,
+      modelorder: [
+        'gpt-4-turbo',
+        'gpt-3.5-turbo',
+        'mistral7b.perplexity',
+        'mistral7b.any',
+        'llama2-70b',
+        'llama2-15b',
+        'group.actionmodels',
+      ],
+    },
+  ],
+  actions: [
+    {
+      name: 'Execute',
+      description: 'Executes the given task.',
+      detailDescription: 'more detailed description',
+      actionPrompt: 'Please run this code',
+    },
+    {
+      name: 'Debug',
+      description: 'Debugs the code.',
+      actionPrompt: 'Please debug this code',
+    },
+  ],
+};
+
+const prompts = [
   {
     type: 'input',
     name: 'projectName',
     message: 'Please enter the name of your application:',
-    default: projectName
+    default: projectName,
   },
   {
     type: 'input',
     name: 'installPath',
     message: 'Please enter the path to install the application:',
-    default: projectName
+    default: projectName,
   },
   {
     type: 'list',
     name: 'template',
     message: 'Please select a template for your application:',
-    choices: templates
+    choices: templates,
   },
   {
     type: 'input',
     name: 'agentDescription',
     message: 'Please enter a description for your agent:',
-    default: 'My Codebolt Agent'
+    default: 'My Codebolt Agent',
   },
-]).then(answers => {
+];
+
+// Add metadata prompts
+if (metadata.agent_routing) {
+  prompts.push({
+    type: 'confirm',
+    name: 'worksonblankcode',
+    message: 'Works on blank code:',
+    default: metadata.agent_routing.worksonblankcode,
+  });
+
+  prompts.push({
+    type: 'confirm',
+    name: 'worksonexistingcode',
+    message: 'Works on existing code:',
+    default: metadata.agent_routing.worksonexistingcode,
+  });
+
+  prompts.push({
+    type: 'checkbox',
+    name: 'softwaredevprocessmanaged',
+    message: 'Managed Software Development Processes:',
+    choices: metadata.agent_routing.softwaredevprocessmanaged,
+  });
+
+  prompts.push({
+    type: 'checkbox',
+    name: 'supportedlanguages',
+    message: 'Supported Languages:',
+    choices: metadata.agent_routing.supportedlanguages,
+  });
+
+  prompts.push({
+    type: 'checkbox',
+    name: 'supportedframeworks',
+    message: 'Supported Frameworks:',
+    choices: metadata.agent_routing.supportedframeworks,
+  });
+}
+
+if (metadata.sdlc_steps_managed) {
+  metadata.sdlc_steps_managed.forEach(step => {
+    prompts.push({
+      type: 'checkbox',
+      name: `${step.name}Instructions`,
+      message: `Example Instructions for ${step.name}:`,
+      choices: step.example_instructions,
+    });
+  });
+}
+
+if (metadata.llm_role) {
+  metadata.llm_role.forEach(role => {
+    prompts.push({
+      type: 'checkbox',
+      name: role.name,
+      message: role.description,
+      choices: role.modelorder,
+    });
+  });
+}
+
+if (metadata.actions) {
+  metadata.actions.forEach(action => {
+    prompts.push({
+      type: 'input',
+      name: `${action.name}ActionPrompt`,
+      message: `${action.name} action prompt:`,
+      default: action.actionPrompt,
+    });
+  });
+}
+
+inquirer.prompt(prompts).then(answers => {
   projectName = answers.projectName.trim();
   const installPath = answers.installPath.trim() === '.' ? process.cwd() : path.resolve(process.cwd(), answers.installPath.trim());
   const selectedTemplate = answers.template;
-  createProject(projectName, installPath, selectedTemplate, answers.agentDescription);
+  createProject(projectName, installPath, selectedTemplate, answers);
 });
 
-function createProject(projectName, installPath, selectedTemplate, description) {
+function createProject(projectName, installPath, selectedTemplate, answers) {
   // Create a project directory with the project name.
   const projectDir = path.resolve(installPath);
   fs.mkdirSync(projectDir, { recursive: true });
@@ -62,11 +242,6 @@ function createProject(projectName, installPath, selectedTemplate, description) 
   const templatePath = path.join(templateDir, selectedTemplate);
   fs.cpSync(templatePath, projectDir, { recursive: true });
 
-  // It is good practice to have dotfiles stored in the
-  // template without the dot (so they do not get picked
-  // up by the starter template repository). We can rename
-  // the dotfiles after we have copied them over to the
-  // new project directory.
   fs.renameSync(
     path.join(projectDir, 'gitignore'),
     path.join(projectDir, '.gitignore')
@@ -77,7 +252,29 @@ function createProject(projectName, installPath, selectedTemplate, description) 
   
   let agentYamlObj = yaml.load(agentYaml);
   agentYamlObj.name = projectName;
-  agentYamlObj.description = description;
+  agentYamlObj.description = answers.agentDescription;
+  agentYamlObj.agent_routing = {
+    worksonblankcode: answers.worksonblankcode,
+    worksonexistingcode: answers.worksonexistingcode,
+    softwaredevprocessmanaged: answers.softwaredevprocessmanaged,
+    supportedlanguages: answers.supportedlanguages,
+    supportedframeworks: answers.supportedframeworks,
+  };
+  agentYamlObj.sdlc_steps_managed = metadata.sdlc_steps_managed.map(step => ({
+    name: step.name,
+    example_instructions: answers[`${step.name}Instructions`],
+  }));
+  agentYamlObj.llm_role = metadata.llm_role.map(role => ({
+    name: role.name,
+    description: role.description,
+    modelorder: answers[role.name],
+  }));
+  agentYamlObj.actions = metadata.actions.map(action => ({
+    name: action.name,
+    description: action.description,
+    detailDescription: action.detailDescription,
+    actionPrompt: answers[`${action.name}ActionPrompt`],
+  }));
   
   agentYaml = yaml.dump(agentYamlObj);
   fs.writeFileSync(agentYamlPath, agentYaml, 'utf8');
--- template/basic/index.js
+++ template/basic/index.js
@@ -3,9 +3,6 @@ const codebolt = require('@codebolt/codeboltjs').default;
 async function execute() {
     await codebolt.waitForConnection();
 
-    // Enter your Code Here. For documentation see: https://docs.codebolt.ai
-    // Example:
-
 }
 
 (async () => { await execute(); })();
