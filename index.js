d02d851bfef5c79f2635ff306ce8372a49382b32
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2024-08-05 16:22:36 +0530

    fix changes

--- index.js
+++ index.js
@@ -21,115 +21,13 @@ projectName = options.name || process.argv[2];
 const templateDir = path.resolve(__dirname, 'template');
 const templates = fs.readdirSync(templateDir).filter(file => fs.statSync(path.join(templateDir, file)).isDirectory());
 
+const agentymlpath = path.join('./template/basic', 'codeboltagent.yaml');
+  let agentYamlData = fs.readFileSync(agentymlpath, 'utf8');
+
+  // Parse the YAML file
+  const parsedYaml = yaml.load(agentYamlData);
 
 
-// Sample metadata object
-// const metadata = {
-//   tags:['basic-agent'],
-//   agent_routing: {
-//     worksonblankcode: true,
-//     worksonexistingcode: true,
-//     softwaredevprocessmanaged: [
-//       'projectplanning',
-//       'codegeneration',
-//       'codetesting',
-//       'codeoptimization',
-//     ],
-//     supportedlanguages: [
-//       'all',
-//       'javascript',
-//       'typescript',
-//       'python',
-//       'go',
-//       'ruby',
-//     ],
-//     supportedframeworks: [
-//       'all',
-//       'nextjs',
-//       'reactjs',
-//       'nodejs',
-//       'express',
-//       'django',
-//     ],
-//   },
-//   defaultagentllm: {
-//     strict: true,
-//     modelorder: ['ollama2'],
-//   },
-//   sdlc_steps_managed: [
-//     {
-//       name: 'codegeneration',
-//       example_instructions: [
-//         'Generate a new React component',
-//         'Create a new API endpoint',
-//       ],
-//     },
-//     {
-//       name: 'deployment',
-//       example_instructions: [
-//         'deploy this file to cloudflare',
-//         'deploy this file to firebase',
-//       ],
-//     },
-//   ],
-//   llm_role: [
-//     {
-//       name: 'documentationllm',
-//       description: 'LLM to be used for advanced Documentation. Please select a model that excels in documentation tasks.',
-//       strict: true,
-//       modelorder: [
-//         'gpt-4-turbo',
-//         'gpt-3.5-turbo',
-//         'mistral7b.perplexity',
-//         'mistral7b.any',
-//         'llama2-70b',
-//         'llama2-15b',
-//         'group.documentationmodels',
-//       ],
-//     },
-//     {
-//       name: 'testingllm',
-//       description: 'LLM to be used for advanced Testing. Please select a model that excels in testing tasks.',
-//       strict: true,
-//       modelorder: [
-//         'gpt-4-turbo',
-//         'gpt-3.5-turbo',
-//         'mistral7b.perplexity',
-//         'mistral7b.any',
-//         'llama2-70b',
-//         'llama2-15b',
-//         'group.testingmodels',
-//       ],
-//     },
-//     {
-//       name: 'actionllm',
-//       description: 'LLM to be used for executing advanced actions.',
-//       strict: true,
-//       modelorder: [
-//         'gpt-4-turbo',
-//         'gpt-3.5-turbo',
-//         'mistral7b.perplexity',
-//         'mistral7b.any',
-//         'llama2-70b',
-//         'llama2-15b',
-//         'group.actionmodels',
-//       ],
-//     },
-//   ],
-//   actions: [
-//     {
-//       name: 'Execute',
-//       description: 'Executes the given task.',
-//       detailDescription: 'more detailed description',
-//       actionPrompt: 'Please run this code',
-//     },
-//     {
-//       name: 'Debug',
-//       description: 'Debugs the code.',
-//       actionPrompt: 'Please debug this code',
-//     },
-//   ],
-// };
 let agent_routing = []
 let defaultagentllm = []
 let sdlc_steps_managed = []
@@ -171,52 +69,43 @@ const prompts = [
     type: 'list',
     name: 'tags',
     message: 'Please select tags:',
-    choices: metadata.tags,
+    choices: parsedYaml.tags,
   },
 ];
 
 // Add metadata prompts
-if (metadata.agent_routing) {
+if (parsedYaml.metadata.agent_routing) {
   prompts.push({
     type: 'confirm',
     name: 'worksonblankcode',
     message: 'Works on blank code:',
-    default: metadata.agent_routing.worksonblankcode,
+    default: parsedYaml.metadata.agent_routing.worksonblankcode,
   });
 
   prompts.push({
     type: 'confirm',
     name: 'worksonexistingcode',
     message: 'Works on existing code:',
-    default: metadata.agent_routing.worksonexistingcode,
-  });
-
-  prompts.push({
-    type: 'list',
-    name: 'softwaredevprocessmanaged',
-    message: 'Managed Software Development Processes:',
-    choices: metadata.agent_routing.softwaredevprocessmanaged,
+    default: parsedYaml.metadata.agent_routing.worksonexistingcode,
   });
 
   prompts.push({
     type: 'list',
     name: 'supportedlanguages',
     message: 'Supported Languages:',
-    choices: metadata.agent_routing.supportedlanguages,
+    choices: parsedYaml.metadata.agent_routing.supportedlanguages,
   });
 
   prompts.push({
     type: 'list',
     name: 'supportedframeworks',
     message: 'Supported Frameworks:',
-    choices: metadata.agent_routing.supportedframeworks,
+    choices: parsedYaml.metadata.agent_routing.supportedframeworks,
   });
 }
 
-
-
-if (metadata.llm_role) {
-  metadata.llm_role.forEach(role => {
+if (parsedYaml.metadata.llm_role) {
+  parsedYaml.metadata.llm_role.forEach(role => {
     prompts.push({
       type: 'list',
       name: role.name,
@@ -226,63 +115,66 @@ if (metadata.llm_role) {
   });
 }
 
-async function askForActions(res) {
-  let addMoreActions = true;
-  while (addMoreActions) {
-    const actionPrompt = [
-      {
-        type: 'input',
-        name: 'actionName',
-        message: 'Please Enter Action Name:',
-      },
-      {
-        type: 'input',
-        name: 'actionDescription',
-        message: 'Please Enter Action Description:',
-      },
-      {
-        type: 'input',
-        name: 'detailDescription',
-        message: 'Please Enter Detail Description (optional):',
-      },
-      {
-        type: 'input',
-        name: 'actionPrompt',
-        message: 'Please Enter Action Prompt (optional):',
-      },
-      {
-        type: 'confirm',
-        name: 'addMoreActions',
-        message: 'Do you want to add more actions?',
-        default: false,
-      }
-    ];
+// async function askForActions(res) {
+//   let addMoreActions = true;
+//   while (addMoreActions) {
+//     const actionPrompt = [
+//       {
+//         type: 'input',
+//         name: 'actionName',
+//         message: 'Please Enter Action Name:',
+//       },
+//       {
+//         type: 'input',
+//         name: 'actionDescription',
+//         message: 'Please Enter Action Description:',
+//       },
+//       {
+//         type: 'input',
+//         name: 'detailDescription',
+//         message: 'Please Enter Detail Description (optional):',
+//       },
+//       {
+//         type: 'input',
+//         name: 'actionPrompt',
+//         message: 'Please Enter Action Prompt (optional):',
+//       },
+//       {
+//         type: 'confirm',
+//         name: 'addMoreActions',
+//         message: 'Do you want to add more actions?',
+//         default: false,
+//       }
+//     ];
 
-    const actionRes = await inquirer.prompt(actionPrompt);
-    res.push({
-      name: actionRes.actionName,
-      description: actionRes.actionDescription,
-      detailDescription: actionRes.detailDescription,
-      actionPrompt: actionRes.actionPrompt,
-    });
-    addMoreActions = actionRes.addMoreActions;
-  }
-  return res;
-}
+//     const actionRes = await inquirer.prompt(actionPrompt);
+//     res.push({
+//       name: actionRes.actionName,
+//       description: actionRes.actionDescription,
+//       detailDescription: actionRes.detailDescription,
+//       actionPrompt: actionRes.actionPrompt,
+//     });
+//     addMoreActions = actionRes.addMoreActions;
+//   }
+//   return res;
+// }
 
-async function askForInstructions(res) {
+async function askForInstructions() {
+ let res= []
   let addMoreInstructions = true;
   while (addMoreInstructions) {
     const additionalPrompt = [
       {
-        type: 'choices',
+        type: 'input',
         name: 'InstructionName',
         message: 'Please Enter SDLC Step Name:',
+        default: parsedYaml.metadata.sdlc_steps_managed.name,
       },
       {
         type: 'input',
         name: 'InstructionDescription',
         message: 'Please Enter Instruction Description:',
+        default:  'Generate a new React component'
       },
       {
         type: 'confirm',
@@ -300,9 +192,10 @@ async function askForInstructions(res) {
   return res;
 }
 
-inquirer.prompt(prompts).then(answers => {
+inquirer.prompt(prompts).then(async answers => {
   let res = [];
-  let instructions = askForInstructions(res)
+  let instructions = await askForInstructions(res)
+  console.log(instructions)
   projectName = answers.projectName.trim();
   const installPath = answers.installPath.trim() === '.' ? process.cwd() : path.resolve(process.cwd(), answers.installPath.trim());
   const selectedTemplate = answers.template;
@@ -350,12 +243,15 @@ function createProject(projectName, installPath, selectedTemplate, answers) {
     modelorder: answers[role.name],
   }));
 
-  agentYamlObj.actions = metadata.actions.map(action => ({
+  agentYamlObj.actions = parsedYaml.actions.map(action => ({
     name: answers[`name${action.name}`],
     description: answers[`description${action.name}`],
     detailDescription: answers[`detailDescription${action.name}`],
     actionPrompt: answers[`actionPrompt${action.name}`],
   }));
+
+  console.log(agentYamlObj)
+  return
   
   agentYaml = yaml.dump(agentYamlObj);
 
